258738cc35fe644a08a283443505e3afa2d7d516
brayner11 <braynerlopez11@hotmail.com>
2024-03-04 16:24:29 +0000

    make it better

--- src/components/Form.js
+++ src/components/Form.js
@@ -16,7 +16,7 @@ const Form = ({newLocation}) => {
         <div className="container">
             <form onSubmit={onSubmit}>
                 <div className="input-group mb-3 mx-auto">
-                    <input type="text" className="form-control" placeholder="Ciudad" onChange={(e) =>setCity(e.target.value)}/>
+                    <input type="text" className="form-control" placeholder="City" onChange={(e) =>setCity(e.target.value)}/>
                     <button className="btn btn-primary input-group-text" type="submit">Search</button>
                 </div>
             </form>
